74e5fae21cb7489fe88befb2d8404f2ca80e52f1
GuilhermeSchreiber7 <guilhermeschreieber2007@gmail.com>
2025-04-15 15:53:43 -0300

    FEAT: Adiciona suporte ao Django REST Framework; cria modelos Autor e Livro com serializers e views correspondentes; registra modelos no admin; atualiza dependências no pyproject.toml.

--- livraria/models.py
+++ livraria/models.py
@@ -19,9 +19,23 @@ class Editora(models.Model):
 class Autor(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(null=True, blank=True)
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
 
     def __str__(self):
         return self.nome.title()
-class Meta:
-        verbose_name = 'Autor'
-        verbose_name_plural = 'Autores'
+    
+class Livro(models.Model):
+    titulo = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True, null=False, blank=False)
+    quantidade = models.IntegerField(default=0)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0.00, null=True, blank=True)
+    Categoria = models.ForeignKey( Categoria, on_delete=models.PROTECT, related_name='livros')
+    Editora = models.ForeignKey( Editora, on_delete=models.PROTECT, related_name='livros')
+    Autor = models.ManyToManyField(Autor, related_name='livros')
+
+    def __str__(self):
+        return f"{self.titulo.title()} - {self.quantidade} unidades - R$ {self.preco}"
+
+
