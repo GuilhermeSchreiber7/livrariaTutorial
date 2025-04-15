ad0f35c547a10f794fea983175500fdbf49cd148
GuilhermeSchreiber7 <guilhermeschreieber2007@gmail.com>
2025-04-15 14:11:36 -0300

    FEAT: Adiciona configuração de idioma e fuso horário para pt-br e America/Sao_Paulo; cria modelos Categoria, Editora e Autor; registra modelos no admin; adiciona arquivo .gitignore.

--- livraria/models.py
+++ livraria/models.py
@@ -1,4 +1,27 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome.title()
+
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome.title()
+    
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome.title()
+class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
