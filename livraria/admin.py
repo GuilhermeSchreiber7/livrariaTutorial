a75479710a4f18706d585a814ef2dfad33fffbfd
GuilhermeSchreiber7 <guilhermeschreieber2007@gmail.com>
2025-04-29 15:01:26 -0300

    FEAT: Remove arquivos desnecessários de configuração e serializers da aplicação

--- livraria/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Categorias, Editora, Autor, Livro
-# Register your models here.
-
-admin.site.register(Categorias)
-admin.site.register(Editora)
-admin.site.register(Autor)
-admin.site.register(Livro)
